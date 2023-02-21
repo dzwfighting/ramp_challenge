37f43c281327e66cf4d468fd0ff60365b88bfca4
Even <yiwen7943@gmail.com>
2023-02-21 06:47:56 +0000

    Fixed

--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -71,7 +71,8 @@ export function InputSelect<TItem>({
                 "RampInputSelect--dropdown-container-opened": isOpen,
               })}
               {...getMenuProps()}
-              style={{ top: dropdownPosition.top, left: dropdownPosition.left }}
+              // fixed bug1
+              style={{ top: dropdownPosition.top, left: dropdownPosition.left, position: "absolute" }}
             >
               {renderItems()}
             </div>
